d7b699d7f2e3a3c247cd8b5509a632e14ab6c180
hyeongseok yun <caa789@naver.com>
2022-12-12 18:04:02 +0900

    [Silver V] Title: 셀프 넘버, Time: 16 ms, Memory: 79772 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.swift
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.swift
@@ -1,60 +1,22 @@
 import Foundation
 
-var firstResult: [Int] = []
-var removedNumber: Set<Int> = []
+var resultSet: Set<Int> = []
+var sum = 0
 
-for firstValue in 1..<10000 {
+for i in 1...10000 {
     
-    firstResult.append(firstValue)
+    sum = sum + i
+    
+    for char in String(i) {
+        sum = sum + char.wholeNumberValue!
+    }
+    resultSet.insert(sum)
+    sum = 0
 }
 
-for secondValue in 1..<10000 {
+for i in 1...10000 {
     
-    var convertedString = String(secondValue)
-    
-    var caculatedResult = 0
-    
-    if convertedString.count == 1 {
-     
-        caculatedResult = secondValue + secondValue
+    if !resultSet.contains(i) {
+        print(i)
     }
-    
-    else if convertedString.count == 2 {
-        
-        let firstChar = convertedString[convertedString.startIndex]
-        let lastChar = convertedString[convertedString.index(before: convertedString.endIndex)]
-        
-        caculatedResult = secondValue + firstChar.wholeNumberValue! + lastChar.wholeNumberValue!
-    }
-    
-    else if convertedString.count == 3 {
-        
-        let firstChar = convertedString[convertedString.startIndex]
-        let middleChar = convertedString[convertedString.index(after: convertedString.startIndex)]
-        let lastChar = convertedString[convertedString.index(before: convertedString.endIndex)]
-        
-        caculatedResult = secondValue + firstChar.wholeNumberValue! + middleChar.wholeNumberValue! + lastChar.wholeNumberValue!
-    }
-    else if convertedString.count == 4 {
-        
-        let firstChar = convertedString[convertedString.startIndex]
-        let afterFirstChar = convertedString[convertedString.index(after: convertedString.startIndex)]
-        let beforLastChar = convertedString[convertedString.index(before: convertedString.index(before: convertedString.endIndex))]
-        let lastChar = convertedString[convertedString.index(before: convertedString.endIndex)]
-
-        caculatedResult = secondValue + firstChar.wholeNumberValue! + afterFirstChar.wholeNumberValue! + beforLastChar.wholeNumberValue! + lastChar.wholeNumberValue!
-    }
-    
-    if removedNumber.contains(caculatedResult) || caculatedResult >= 10000{
-        continue
-    }
-    firstResult.remove(at: firstResult.firstIndex(of: caculatedResult)!)
-    
-    removedNumber.insert(caculatedResult)
-    
-}
-
-for result in firstResult {
-    
-    print(result)
 }
